a9a8b2ddf9f3e131324c8711d4c230315e0c544a
Dagore <david.gomez.redondo.dev@gmail.com>
2022-02-17 14:20:17 +0100

    Videojuegos - Colección

---
+++ EntregaRelacion2VideojuegoColeccion/Coleccion.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace VideojuegosDB
+{
+    class Coleccion
+    {
+        /*ATRIBUTOS*/
+        List<Videojuego> lista = new List<Videojuego>();
+        
+        /*PROPIEDADES*/
+        /// <summary>
+        /// Número de videojuegos en la colección.
+        /// </summary>
+        public int NumeroVideojuegos
+        {
+            get
+            {
+                return lista.Count;
+            }
+        }
+        
+        /*MÉTODOS*/
+        
+        /*MANIPULACIÓN DE LA LISTA*/
+        /// <summary>
+        /// Inserta un objeto Videojuego en la colección.
+        /// </summary>
+        /// <param name="v">Instancia de videojuego.</param>
+        public void InsertaVideojuego(Videojuego v)
+        {
+            lista.Add(v);
+        }
+        /// <summary>
+        /// Elimina un videojuego de la colección usando su posición.
+        /// </summary>
+        /// <param name="position">Posición en la lista del videojuego que se quiere eliminar.</param>
+        public void EliminaVideojuego(int position)
+        {
+            if (position >= 0 && position < NumeroVideojuegos)
+            {
+                lista.RemoveAt(position);
+            }
+            else
+            {
+                throw new Exception("Esa posición no existe en la colección.");
+            }
+        }
+        /// <summary>
+        /// Obtiene un videojuego de la lista a través de su posición.
+        /// </summary>
+        /// <param name="position">Posición en la lista para el videojuego.</param>
+        /// <returns>Instancia de Videjuego</returns>
+        public Videojuego LeeVideojuego(int position)
+        {
+            if (position >= 0 && position < NumeroVideojuegos)
+            {
+                return lista[position];
+            }
+            else
+            {
+                throw new Exception("Esa posición no existe en la colección.");
+            }
+        }
+       
+        /*GESTIÓN DE FICHEROS*/
+        /// <summary>
+        /// Vuelca la lista en un fichero de texto, machacándolo.
+        /// </summary>
+        /// <param name="path"></param>
+        public void GuardaColeccionFichero(string path)
+        {
+            Videojuego videojuego;
+            StreamWriter sw = new StreamWriter(path);
+
+            for (int i = 0; i < NumeroVideojuegos; i++)
+            {
+                videojuego = lista[i];
+                sw.WriteLine(videojuego.ToCSVString());
+            }
+
+            sw.Close();
+        }
+        /// <summary>
+        /// Obtiene la lista de videojuegos de un fichero.
+        /// </summary>
+        /// <param name="path">Ruta del fichero.</param>
+        public void LeeColeccionFichero(string path)
+        {
+            Videojuego videojuego;
+            StreamReader sr;
+            Plataforma plataforma;
+            TipoJuego genero;
+            string nombre;
+            int anno, valoracion;
+            string[] record;
+
+            sr = new StreamReader(path);
+
+            while (!sr.EndOfStream)
+            {
+                record = sr.ReadLine().Split(';');
+
+                nombre = record[0];
+                anno = int.Parse(record[1]);
+                plataforma = (Plataforma)int.Parse(record[2]);
+                genero = (TipoJuego)int.Parse(record[3]);
+                valoracion = int.Parse(record[4]);
+
+                videojuego = new Videojuego(nombre, anno, plataforma, genero, valoracion);
+                lista.Add(videojuego);
+            }
+
+            sr.Close();
+        }
+
+        /*OVERRRIDE*/
+        public override string ToString()
+        {
+            int tableWidth = 115;
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(NewLine(tableWidth));
+            sb.AppendLine(NewRow(tableWidth, "Videojuego", "Año", "Plataforma", "Tipo de juego", "Val."));
+            
+            for (int i = 0; i < NumeroVideojuegos; i++)
+            {
+                sb.AppendLine(NewLine(tableWidth));
+                sb.AppendLine(NewRow(tableWidth, lista[i].ToString().Split(';')));
+            }
+
+            return sb.ToString();
+        }
+
+        /*REPRESENTACIONES*/
+        /// <summary>
+        /// Escribe una lista ordenada de los videojuegos en la lista.
+        /// </summary>
+        /// <returns>Un string con cada videojuego en una línea y una viñeta numérica.</returns>
+        public string ToStringNum()
+        {
+            StringBuilder sb = new StringBuilder();
+            int mark = 1;
+
+            for (int i = 0; i < NumeroVideojuegos; i++)
+            {
+                sb.AppendLine($"{mark}- {lista[i].Nombre}");
+                mark++;
+            }
+
+            return sb.ToString();
+        }
+
+        /*HELPERS*/
+        /// <summary>
+        /// Genera una nueva linea separadora del ancho especificada.
+        /// </summary>
+        /// <param name="tableWidth">Un entero que indica el número de caracteres del separador.</param>
+        /// <returns>String con el separador generado</returns>
+        private string NewLine(int tableWidth)
+        {
+            return new string('-', tableWidth);
+        }
+        /// <summary>
+        /// Genera una nueva fila de la tabla con un ancho especificado y tantas columnas como argurmentos.
+        /// </summary>
+        /// <param name="tableWidth">Un entero que indica el ancho de la fila.</param>
+        /// <param name="columns">Una serie de strings, cada uno se sitúa centrado en una columna de la fila</param>
+        /// <returns>Un string con la nueva línea generada.</returns>
+        private string NewRow(int tableWidth, params string[] columns)
+        {
+            int width = (tableWidth - columns.Length) / columns.Length;
+            StringBuilder row = new StringBuilder("|");
+
+            foreach (string column in columns)
+            {
+                row.Append(AlignCentre(column, width)).Append("|");
+            }
+
+            return row.ToString();
+        }
+        /// <summary>
+        /// Centra una cadena de texto con espacios a los lados para un ancho especifico.
+        /// </summary>
+        /// <param name="text">Un string con el texto que se quiere centrar.</param>
+        /// <param name="width">Ancho de referencia para centrar el texto</param>
+        /// <returns>Un string con el nuevo texto centrado.</returns>
+        private string AlignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }
+
+    }
+}
---
+++ EntregaRelacion2VideojuegoColeccion/Videojuego.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace VideojuegosDB
+{
+    class Videojuego
+    {
+        /*ATRIBUTOS*/
+        private string nombre;
+        private int anno;
+        private Plataforma plataforma;
+        private TipoJuego genero;
+        private int valoracion;
+
+        /*CONSTRUCTORES*/
+        /// <summary>
+        /// Genera un nuevo objeto Videojuego con varias característas.
+        /// </summary>
+        /// <param name="nombre">Nombre del videojuego, no puede estar vacío.</param>
+        /// <param name="anno">Año de la lanzamiento, entre 1970 y 2100.</param>
+        /// <param name="plataforma">Plataforma en la que salió el juego. PC, PlayStation_4, Xbox_One o Switch.</param>
+        /// <param name="genero">Accion, Aventura, Puzzle, Rol, Deportes o Estrategia.</param>
+        /// <param name="valoracion">Un entero entre 0 y 100. 0 es la valoración más baja y 100 la más alta.</param>
+        public Videojuego(string nombre, int anno, Plataforma plataforma, TipoJuego genero, int valoracion)
+        {
+            Nombre = nombre;
+            Anno = anno;
+            Plataforma = plataforma;
+            Genero= genero;
+            Valoracion = valoracion;
+        }
+
+        /*PROPIEDADES*/
+        /// <summary>
+        /// Nombre del videojuego, no puede estar vacío.
+        /// </summary>
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                if (value != "")
+                {
+                    nombre = value;
+                }
+                else
+                {
+                    throw new Exception("Eñ nombre del videojuego no puede estar vacío.");
+                }
+            }
+        }
+        /// <summary>
+        /// Año de lanzamiento, entre 1970 y 2100.
+        /// </summary>
+        public int Anno
+        {
+            get
+            {
+                return anno;
+            }
+            set
+            {
+                if (value >= 1970 && value <= 2100)
+                {
+                    anno = value;
+                }
+                else
+                {
+                    throw new Exception("El año está comprendido entre 1970 y 2100.");
+                }
+            }
+        }
+        /// <summary>
+        /// Plataforma: PC, PlayStation_4, Xbox_One, Switch.
+        /// </summary>
+        public Plataforma Plataforma
+        {
+            get
+            {
+                return plataforma;
+            }
+            set
+            {
+                plataforma = value;
+            }
+        }
+        /// <summary>
+        /// Genero: Accion, Aventura, Puzzle, Rol, Deportes, Estrategia.
+        /// </summary>
+        public TipoJuego Genero
+        {
+            get
+            {
+                return genero;
+            }
+            set
+            {
+                genero = value;
+            }
+        }
+        /// <summary>
+        /// Entero entre 0 y 100. 0 se corresponde con la peor valoración y 100 la mejor.
+        /// </summary>
+        public int Valoracion
+        {
+            get
+            {
+                return valoracion;
+            }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    valoracion = value;
+                }
+                else
+                {
+                    throw new Exception("La valoración debe estar entre 0 y 100");
+                }
+            }
+        }
+
+        /*OVERRIDE*/
+        public override string ToString()
+        {
+            
+            return $"{Nombre};{Anno};{Plataforma};{Genero};{Valoracion}";
+        }
+        
+        /*REPRESENTACIÓN*/
+        public string ToCSVString()
+        {
+            return $"{Nombre};{Anno};{(int)Plataforma};{(int)Genero};{Valoracion}";
+        }
+    }
+}
