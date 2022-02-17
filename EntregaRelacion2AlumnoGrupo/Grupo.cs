4043786d9bb99700626eed50d6ceecfabab5f76e
Dagore <david.gomez.redondo.dev@gmail.com>
2022-02-17 12:21:33 +0100

    Relación 2 POO

--- EntregaRelacion2AlumnoGrupo/Grupo.cs
+++ EntregaRelacion2AlumnoGrupo/Grupo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace EntregaRelacion2AlumnoGrupo
@@ -8,6 +9,15 @@ namespace EntregaRelacion2AlumnoGrupo
     {
         /*ATRIBUTOS*/
         private List<Alumno> listaAlumnos = new List<Alumno>();
+
+        /*PROPIEDADES*/
+        public int NumeroAlumnos
+        {
+            get
+            {
+                return listaAlumnos.Count;
+            }
+        }
 
         /*MÉTODOS*/
         /// <summary>
@@ -34,7 +44,130 @@ namespace EntregaRelacion2AlumnoGrupo
         /// <param name="path">Ruta del fichero.</param>
         public void EscribeFicheroBinario (string path)
         {
+            FileStream fs = new FileStream(path, FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+            Alumno alumno;
 
+            bw.Write(NumeroAlumnos);
+
+            for (int i = 0; i < NumeroAlumnos; i++)
+            {
+                alumno = listaAlumnos[i];
+
+                bw.Write(alumno.Nombre);
+                bw.Write(alumno.Edad);
+                bw.Write(alumno.Calificacion);
+            }
+
+            bw.Close();
+            fs.Close();
+        }
+        /// <summary>
+        /// Limpia la lista y la rellena desde el fichero binario indicado
+        /// </summary>
+        /// <param name="path">Ruta del fichero</param>
+        public void LeeFicheroBinario (string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Open);
+            BinaryReader br = new BinaryReader(fs);
+            Alumno alumno;
+            int length = br.ReadInt32();
+            listaAlumnos.Clear();
+
+            for (int i = 0; i < length; i++)
+            {
+                alumno = new Alumno(br.ReadString(), br.ReadInt32(), br.ReadDouble());
+
+                listaAlumnos.Add(alumno);
+            }
+
+            br.Close();
+            fs.Close();
+        }
+        /// <summary>
+        /// Escribe la lista en el fichero de texto indicado.
+        /// </summary>
+        /// <param name="path">Ruta del fichero</param>
+        public void EscribeFicheroTexto (string path)
+        {
+            StreamWriter sw = new StreamWriter(path);
+            Alumno alumno;
+
+            sw.WriteLine(NumeroAlumnos);
+
+            for (int i = 0; i < NumeroAlumnos; i++)
+            {
+                alumno = listaAlumnos[i];
+
+                sw.WriteLine(alumno.Nombre);
+                sw.WriteLine(alumno.Edad);
+                sw.WriteLine(alumno.Calificacion);
+            }
+
+            sw.Close();
+        }
+        /// <summary>
+        /// Limpia la lista y la rellena desde el fichero de texto indicado.
+        /// </summary>
+        /// <param name="path">Ruta del fichero</param>
+        public void LeeFicheroTexto (string path)
+        {
+            StreamReader sr = new StreamReader(path);
+            Alumno alumno;
+            int length = int.Parse(sr.ReadLine());
+
+            for (int i = 0; i < length; i++)
+            {
+                alumno = new Alumno(sr.ReadLine(), int.Parse(sr.ReadLine()), double.Parse(sr.ReadLine()));
+
+                listaAlumnos.Add(alumno);
+            }
+
+            sr.Close();
+        }
+        /// <summary>
+        /// Escribe la lista en el fichero CSV indicado.
+        /// </summary>
+        /// <param name="path">Ruta del fichero.</param>
+        public void EscribeFicheroCSV(string path)
+        {
+            StreamWriter sw = new StreamWriter(path);
+            Alumno alumno;
+
+            for (int i = 0; i < NumeroAlumnos; i++)
+            {
+                alumno = listaAlumnos[i];
+
+                sw.WriteLine($"{alumno.Nombre};{alumno.Edad};{alumno.Calificacion}");
+            }
+
+            sw.Close();
+        }
+        /// <summary>
+        /// Limpia la lista y la rellena desde el fichero CSV indicado.
+        /// </summary>
+        /// <param name="path">Ruta del fichero.</param>
+        public void LeeFicheroCSV (string path)
+        {
+            StreamReader sr = new StreamReader(path);
+            Alumno alumno;
+            string[] record;
+
+            while (!sr.EndOfStream)
+            {
+                record = sr.ReadLine().Split(";");
+                alumno = new Alumno(record[0], int.Parse(record[1]), double.Parse(record[2]));
+                listaAlumnos.Add(alumno);
+            }
+
+            sr.Close();
+        }
+        public void ForEach (Action<Alumno> action)
+        {
+            for (int i = 0; i < NumeroAlumnos; i++)
+            {
+                action(listaAlumnos[i]);
+            }
         }
 
         /*OVERRIDE*/
--- EntregaRelacion2AlumnoGrupo/Program.cs
+++ EntregaRelacion2AlumnoGrupo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace EntregaRelacion2AlumnoGrupo
 {
@@ -6,7 +7,142 @@ namespace EntregaRelacion2AlumnoGrupo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Grupo grupo = new Grupo();
+            int option;
+
+            ShowMenu();
+
+            option = int.Parse(Console.ReadLine());
+
+            while (option > 0 && option < 9)
+            {
+                switch (option)
+                {
+                    case 1:
+                        {
+                            LeeAlumnoConsola(grupo);
+                        }
+                        break;
+                    case 2:
+                        {
+                            ImprimeAlumnos(grupo);
+                        }
+                        break;
+                    case 3:
+                        {
+                            grupo.EscribeFicheroBinario("fichero/registroalumnosbinario.bin");
+                        }
+                        break;
+                    case 4:
+                        {
+                            grupo.LeeFicheroBinario("fichero/registroalumnosbinario.bin");
+                        }
+                        break;
+                    case 5:
+                        {
+                            grupo.EscribeFicheroTexto("fichero/registroalumnostexto.txt");
+                        }
+                        break;
+                    case 6:
+                        {
+                            grupo.LeeFicheroTexto("fichero/registroalumnostexto.txt");
+                        }
+                        break;
+                    case 7:
+                        {
+                            grupo.EscribeFicheroCSV("fichero/registroalumnos.csv");
+                        }
+                        break;
+                    case 8:
+                        {
+                            grupo.LeeFicheroCSV("fichero/registroalumnos.csv");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.Write("Introduzca otra opción o 0 para terminar: ");
+                option = int.Parse(Console.ReadLine());
+            }
+        }
+        #region utils
+        static void ShowMenu()
+        {
+            Console.WriteLine("MENU:");
+
+            Console.WriteLine("1 - LeeAlumnoConsola");
+            Console.WriteLine("2 - ImprimeAlumnos");
+            Console.WriteLine("3 - EscribeFicheroBinario");
+            Console.WriteLine("4 - LeeFicheroBinario");
+            Console.WriteLine("5 - EscribeFicheroTexto");
+            Console.WriteLine("6 - LeeFicheroTexto");
+            Console.WriteLine("7 - EscribeFicheroCSV");
+            Console.WriteLine("8 - LeeFicheroCSV");
+
+            Console.Write("Introduce una opción: ");
+        }
+        static void PrintLine(int tableWidth)
+        {
+            Console.WriteLine(new string('-', tableWidth));
+        }
+        static void PrintRow(int tableWidth, params string[] columns)
+        {
+            int width = (tableWidth - columns.Length) / columns.Length;
+            string row = "|";
+
+            foreach (string column in columns)
+            {
+                row += AlignCentre(column, width) + "|";
+            }
+
+            Console.WriteLine(row);
+        }
+        static string AlignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }
+        #endregion
+        static void LeeAlumnoConsola (Grupo grupo)
+        {
+            string nombre;
+            int edad;
+            double calificacion;
+
+            Console.WriteLine("Introduce la información del nuevo alumno:");
+
+            Console.Write("Nombre del alumno: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Edad del alumno: ");
+            edad = int.Parse(Console.ReadLine());
+
+            Console.Write("Calificación del alumno: ");
+            calificacion = double.Parse(Console.ReadLine());
+
+            grupo.InsertaAlumnoLista(nombre, edad, calificacion);
+        }
+        static void ImprimeAlumnos (Grupo grupo)
+        {
+            int tableWidth = 100;
+
+            PrintLine(tableWidth);
+            PrintRow(tableWidth, "Nombre", "Edad", "Calificación");
+
+            grupo.ForEach(alumno => 
+            {
+                PrintLine(tableWidth);
+                PrintRow(tableWidth, alumno.Nombre, alumno.Edad.ToString(), String.Format("{0:0.00}", alumno.Calificacion));
+            });
         }
     }
 }
